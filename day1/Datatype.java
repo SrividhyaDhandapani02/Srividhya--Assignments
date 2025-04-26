07bfc1aa023886a8fdc30d7b1fca35b4a0f72aa3
SrividhyaDhandapani02 <vidhya.2d@gmail.com>
2025-04-26 18:26:08 -0400

    Add files via upload
    day1 assignment

---
+++ day1/Datatype.java
@@ -0,0 +1,25 @@
+package Week1.day1;
+
+public class Datatype {
+public static void main(String[] args) {
+	byte age=30;
+	int salary=300000;
+	float varfloat=3.15555F;
+	short varshort=5555;
+	long varlong=1234569874563214L;
+	boolean varboolean=true;
+	char varchar='a';
+	double vardouble= 4.5555555;
+	String name= "Vidhya";
+	System.out.println(age);
+	System.out.println(varshort);
+	System.out.println(salary);
+	System.out.println(varfloat);
+	System.out.println(varlong);
+	System.out.println(varboolean);
+	System.out.println(varchar);
+	System.out.println(name);
+	System.out.println(vardouble);
+	
+}
+}
